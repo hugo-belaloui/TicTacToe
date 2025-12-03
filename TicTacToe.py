8a6a5934c6654b7d2052537237a8ab8389ed9e0a
hugo-belaloui <hugo.belaloui@laplateforme.io>
2025-12-03 09:27:22 +0100

    minor changes

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,6 @@
-cases = [1,2,3,4,5,6,7,8,9] #initialiser ma grille de neuf cases
+cases = [1,2,3,
+        4,5,6,
+        7,8,9] #initialiser ma grille de neuf cases
 
 def affichage(): #fonction pour afficher ma grille
         for i in range(0, len(cases), 3): #boucle pour afficher le contenu de ma grille sur trois ligne et d'avancer par trois cases
@@ -10,19 +12,26 @@ affichage()
 
 #choix d'une case par les joueurs a tour de role
 choix = 0
-print("choisissez une case entre 1 et 9")
-choix = int(input("joueur X : "))
-cases[choix-1]="X"
-affichage()
-print("choisissez une case entre 1 et 9")
-choix = int(input("joueur X : "))
-cases[choix-1]="O"
-affichage()
+def entree_choix(): #boucle pour gérer les conditions d'input et les erreurs
+    while True: 
+            print("Choisissez une case entre 1 et 9")
+            n_choix = int(input("joueur X : ")) 
+            if  1 <= n_choix <= 9 : #verifie la plage
+                if isinstance(cases[n_choix-1], int): #verifie qu'il s'agit d'un int
+                    return n_choix
+                else : 
+                    print("case déja prise, réessayez")
+            elif ValueError: #il ne s'agit pas d'un int
+                print("Erreur, la valeur ne peut pas etre un string et doit etre un chiffre entre 1 et 9")
+            else :
+                print("Erreur, le chiffre doit etre entre 1 et 9")
+
+choix = entree_choix()
 
 #conditions necessaire :
 #ne peut choisir qu'un chiffre entre 1 et 9
 #ne peut choisir un chiffre déja choisi
-#input ne peut etre qu'un integer 
+#input ne peut etre qu'un integer, renvoie une erreur 
 #si allignement fin partie 
 #si plus de cases disponible fin partie
 #boucle tant qu'un condition n'est pas remplie
